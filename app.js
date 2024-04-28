92fee0eb1e5c54cd579d89132a964cbb60f1bc96
Lolanimus <94278422+Lolanimus@users.noreply.github.com>
2024-04-28 10:07:47 -0400

    Updated set(key, value) function, so that it doubles the number of max buckets in the hash table when 75% of hash table is full

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 class HashTable {
     constructor() {
         this.hashTable = [];
+        this.maxBuckets = 16;
+        this.capacity = 0;
     }
 
     hash(key) {
         let hashCode = 0;
         let moduloResult = 0;
-
         const primeNumber = 31;
         for (let i = 0; i < key.length; i++) {
             hashCode = primeNumber * hashCode + key.charCodeAt(i);
-            moduloResult += hashCode % 16;
+            moduloResult += hashCode % this.maxBuckets;
         }
 
-        moduloResult %= 16;
+        moduloResult %= this.maxBuckets;
 
         return moduloResult;
     }
     
     set(key, value) {
+        const loadFactor = 0.75;
         let bucketNumber = this.hash(key);
         this.hashTable[bucketNumber] = value;
+        this.capacity++;
+        if (this.capacity > (loadFactor * this.maxBuckets)) this.maxBuckets *= 2;
     }
 
     has(key) {
@@ -98,7 +102,18 @@ let ht1 = new HashTable();
 ht1.set("Artem", 1);
 ht1.set("Barbara", 2);
 ht1.set("Carlos", 3);
-
-ht1.remove("Artem");
+ht1.set("Artemdd", 1);
+ht1.set("Artessm", 1);
+ht1.set("Artesdfm", 1);
+ht1.set("Ardsfssatem", 1);
+ht1.set("Artsdfaffdsem", 1);
+ht1.set("Aredfsfsfsfsfstem", 1);
+ht1.set("Art1232em", 1);
+ht1.set("Arteasdm", 1);
+ht1.set("222Artem", 1);
+ht1.set("Arte333m", 1);
+ht1.set("Arte333asdam", 1);
+ht1.set("zzxczrte333m", 1);
+ht1.set("Artasdasdasdsadasdasde333m", 10);
 let entries = ht1.entries();
-console.log(entries);
+console.log(ht1);
